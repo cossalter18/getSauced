36203bc4669592d562f878a28b471dabb4ba24ea
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-30 13:04:32 -0500

    working on new post componnet

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { TextField } from "@material-ui/core";
-import { Button } from "@material-ui/core";
+import Button from 'react-bootstrap/Button'
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { TextareaAutosize } from '@material-ui/core'
+
 
 import './NewPost.css'
 
@@ -18,32 +20,34 @@ const myTheme = createMuiTheme({
 
 class NewPost extends Component {
 
-    state = {
-        newPost: {
-          id: '',
-            title: '',
-            body: '',
-            user_id: this.props.reduxState.user.id,
-            username: this.props.reduxState.user.username
-        }
+  state = {
+    newPost: {
+      id: '',
+      title: '',
+      body: '',
+      user_id: this.props.reduxState.user.id,
+      username: this.props.reduxState.user.username
     }
+  }
 
   handleChange = (event, property) => {
     console.log(event.target.value);
     this.setState({
-        newPost: {
-            ...this.state.newPost,
-            [property]: event.target.value
-        }
+      newPost: {
+        ...this.state.newPost,
+        [property]: event.target.value
+      }
     });
   };
 
   postBlog = () => {
     console.log("clicked");
     this.props.dispatch({
-        type: 'NEW_POST',
-        payload: this.state.newPost
+      type: 'NEW_POST',
+      payload: this.state.newPost
     })
+    this.props.history.push('/info')
+
   };
 
   render() {
@@ -53,30 +57,29 @@ class NewPost extends Component {
         <p>{JSON.stringify(this.props.reduxState.user)}</p>
         <div className="inputs">
           <MuiThemeProvider theme={myTheme}>
-            <TextField className="title"
-              type="text"
-              color="primary"
-              name="title"
-              placeholder="Title"
-              onChange={(event) => this.handleChange(event, "title")}
-            ></TextField>
-            <br/>
-            <TextField className="body"
-            aria-label="minimum height" 
-            rows={25} 
-            placeholder ="Start typing..."
-            onChange={(event) => this.handleChange(event, "body")}/>
-            
-            <Button
-              className="button"
-              color="primary"
-              variant="contained"
-              onClick={this.postBlog}
-            >
-              Post!
-            </Button>
+            <div className='title'>
+              <TextField className="title"
+                
+                type="text"
+                color="primary"
+                name="title"
+                placeholder="Title"
+                onChange={(event) => this.handleChange(event, "title")}
+              ></TextField>
+            </div>
+            <br />
+            <div>
+              <TextField className="body"
+                variant="outlined"
+                rows={10}
+                aria-label="minimum height"
+                placeholder="Ravioli Ravioli, give me the formuoli..."
+                onChange={(event) => this.handleChange(event, "body")} />
+            </div>
+            <Button className="postButton" onClick={this.postBlog}>Share the secrets</Button>
           </MuiThemeProvider>
         </div>
+        
       </div>
     );
   }
